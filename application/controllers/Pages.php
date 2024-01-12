706f7278a9f7edb494ef86659d8480e67a12080c
insung5189 <insung5189@gmail.com>
2024-01-12 18:02:28 +0900

    tutorial init

---
+++ application/controllers/Pages.php
@@ -0,0 +1,16 @@
+<?php
+class Pages extends CI_Controller {
+
+        public function view($page = 'home') {
+        if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php')) {
+                // Whoops, we don't have a page for that!
+                phpinfo();
+        }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('pages/'.$page, $data);
+        $this->load->view('templates/footer', $data);
+        }
+}
